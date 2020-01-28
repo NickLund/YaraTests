c4422be2e6ce4983830937e14fd83667b951cc0c
NickLund <trojanscout@gmail.com>
2020-01-28 15:08:23 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,7 @@
+import yara
+
+# File containing rules
+#rules = yara.compile(filepath='/foo/bar/myrules')
+# Compile directly from string
+#rules = yara.compile(source='rule dummy { condition: true }')
 
